9cb129bd827b3022617afdbd39a8f20d43f39ccf
Sergio <sbouden16@gmail.com>
2021-03-31 16:33:22 +0200

    Add _forms, helpers for logic.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,25 @@
 module PostsHelper
+  def if_session; end
+
+  def user_post(_post)
+    if user_signed_in?
+      content_tag :table do
+        content_tag :thead do
+          content_tag :tr do
+            (content_tag :th, "Name") +
+              (content_tag :th, "Title") +
+              (content_tag :th, "Body") +
+              (content_tag :th, colspan: '3')
+          end
+        end
+      end
+      content_tag :tbody do
+        content_tag :tr do
+          (content_tag :td, _post.user.email) +
+          (content_tag :td, _post.title) +
+            (content_tag :td, _post.body)
+        end
+      end
+    end
+  end
 end
